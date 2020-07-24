4d84e51af6ba7873a226d597dd2f881ee588aea0
shay cohen <shayc@steamcc.com>
2020-07-24 13:27:15 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const electron = window.require('electron');
+
+function App() {
+
+
+
+
+
+    return (
+      <div className="App">
+
+      </div>
+    );
+}
+
+export default App;
